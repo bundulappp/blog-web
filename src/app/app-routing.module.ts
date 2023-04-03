01f9db51a3c2b4248f6192b5aa1adb0df02eebd3
Patrik Bundula <bundulappp@gmail.com>
2023-04-03 20:47:17 +0200

    main modules added with lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'main',
+    pathMatch: 'full',
+  },
+  {
+    path: 'main',
+    loadChildren: () =>
+      import('./feature/main/main.module').then((m) => m.MainModule),
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./feature/auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./feature/notfound/notfound.module').then(
+        (m) => m.NotfoundModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/feature/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+
+@NgModule({
+  declarations: [RegisterComponent, LoginComponent, ProfileComponent],
+  imports: [CommonModule, AuthRoutingModule],
+})
+export class AuthModule {}
