f00337cb6e0a4d2b8d5c31bfaa83ec6be0798d94
Patrik Bundula <bundulappp@gmail.com>
2023-04-03 21:45:02 +0200

    add shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, HttpClientModule, ToastrModule.forRoot()],
+  exports: [HttpClientModule],
+})
+export class SharedModule {}
