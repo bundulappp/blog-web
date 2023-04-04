88801f57aa6cd906ea87d2c378e706142411a794
Patrik Bundula <bundulappp@gmail.com>
2023-04-04 21:20:10 +0200

    add header

--- src/app/feature/main/main.module.ts
+++ src/app/feature/main/main.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
-
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
-  declarations: [
-    MainComponent
-  ],
-  imports: [
-    CommonModule,
-    MainRoutingModule
-  ]
+  declarations: [MainComponent],
+  imports: [CommonModule, MainRoutingModule, SharedModule],
 })
-export class MainModule { }
+export class MainModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
   imports: [CommonModule, HttpClientModule, ToastrModule.forRoot()],
-  exports: [HttpClientModule],
+  exports: [HttpClientModule, HeaderComponent],
 })
 export class SharedModule {}
