7e79beceadc0866a6db5b910c37deec44ea9942d
Patrik Bundula <bundulappp@gmail.com>
2023-04-10 19:20:26 +0200

    refil auth service

--- src/app/feature/auth/service/auth.service.ts
+++ src/app/feature/auth/service/auth.service.ts
@@ -5,21 +5,38 @@ import { Observable, tap } from 'rxjs';
 import { environment } from 'src/app/environments/environment';
 import { UserViewModel } from 'src/app/shared/models/auth/UserViewModel';
 import { LoginRequestModel } from 'src/app/shared/models/auth/LoginRequestModel';
+import { UserRegisterViewModel } from 'src/app/shared/models/auth/UserRegisterViewModel';
+import { StorageService } from './localstorage.service';
+import { Router } from '@angular/router';
+import { UserUpdateRequestModel } from 'src/app/shared/models/auth/UserUpdateRequestModel';
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  userRegisterKey: string = 'user_registered_at';
+  userLoginKey: string = 'user';
+  userInformation: UserViewModel | undefined;
+
+  constructor(private http: HttpClient, private storageService: StorageService, private router: Router) {}
 
   getToken(): string | null {
     return localStorage.getItem('token');
   }
 
-  register(user: RegistrationRequestModel): Observable<void> {
-    return this.http.post<void>(
-      environment.apiUrl + 'auth/local/register',
-      user
-    );
+  register(user: RegistrationRequestModel): Observable<UserRegisterViewModel> {
+    return this.http
+      .post<UserRegisterViewModel>(
+        environment.apiUrl + '/auth/local/register',
+        user
+      )
+      .pipe(
+        tap((userData: UserRegisterViewModel) => {
+          this.storageService.setItem(
+            this.userRegisterKey,
+            `${userData.user.createdAt}`
+          );
+        })
+      );
   }
 
   login(loginData: LoginRequestModel): Observable<UserViewModel> {
@@ -27,10 +44,31 @@ export class AuthService {
       .post<UserViewModel>(environment.apiUrl + '/auth/local', loginData)
       .pipe(
         tap((userData) => {
-          console.log(userData);
-          localStorage.setItem('user', JSON.stringify(userData));
+          this.setCurrentUser(userData);
         })
       );
     //https://www.learnrxjs.io/learn-rxjs/operators/multicasting/sharereplay
+  }
+
+  logout(): void {
+    this.storageService.removeItem(this.userLoginKey);
+    this.userInformation = undefined;
+    this.router.navigate(['/auth/login']);
+  }
+
+  userUpdate(userData: UserUpdateRequestModel): Observable<UserViewModel> {
+    return this.http
+      .put<UserViewModel>(environment.apiUrl + '/user/me', userData)
+      .pipe(
+        tap((userData: UserViewModel) => {
+          this.setCurrentUser(userData);
+        })
+      );
+  }
+
+  setCurrentUser(userData: UserViewModel): void {
+    this.storageService.clear();
+    this.userInformation = userData;
+    this.storageService.setItem(this.userLoginKey, JSON.stringify(userData));
   }
 }
