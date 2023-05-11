651fbb67c735c82f5de848c6246398933f8b7c08
Patrik Bundula <bundulappp@gmail.com>
2023-05-11 18:07:03 +0200

    init nav-bar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, NavBarComponent],
   imports: [CommonModule, HttpClientModule, ToastrModule.forRoot()],
-  exports: [HttpClientModule, HeaderComponent, FooterComponent],
+  exports: [
+    HttpClientModule,
+    HeaderComponent,
+    FooterComponent,
+    NavBarComponent,
+  ],
 })
 export class SharedModule {}
