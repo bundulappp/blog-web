7d1c840b9f9accca8604180a582baf3dd53e037e
Patrik Bundula <bundulappp@gmail.com>
2023-05-16 17:05:47 +0200

    add color and nav link bar

---
+++ src/app/shared/header/nav-links-mob/nav-links-mob.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-links-mob',
+  templateUrl: './nav-links-mob.component.html',
+  styleUrls: ['./nav-links-mob.component.scss']
+})
+export class NavLinksMobComponent {
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,9 +5,10 @@ import { ToastrModule } from 'ngx-toastr';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NavLinksMobComponent } from './header/nav-links-mob/nav-links-mob.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, NavBarComponent],
+  declarations: [HeaderComponent, FooterComponent, NavBarComponent, NavLinksMobComponent],
   imports: [CommonModule, HttpClientModule, ToastrModule.forRoot()],
   exports: [
     HttpClientModule,
