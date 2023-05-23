26f96d7c77ef405e394c84ac881bb009441632a7
Patrik Bundula <bundulappp@gmail.com>
2023-05-23 17:59:29 +0200

    init topics-nav

---
+++ src/app/shared/topics-nav/topics-nav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topics-nav',
+  templateUrl: './topics-nav.component.html',
+  styleUrls: ['./topics-nav.component.scss']
+})
+export class TopicsNavComponent {
+
+}
