2f5e6f793c76022555bc466c83ee5873831a47f3
Patrik Bundula <patrik.bundula@gammaorg.hu>
2023-05-25 15:50:59 +0200

    change layout

--- src/app/feature/main/main.module.ts
+++ src/app/feature/main/main.module.ts
@@ -4,9 +4,24 @@ import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import {
+  MenuFoldOutline,
+  MenuUnfoldOutline,
+} from '@ant-design/icons-angular/icons';
+import { IconDefinition } from '@ant-design/icons-angular';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
+
+const icons: IconDefinition[] = [MenuFoldOutline, MenuUnfoldOutline];
 
 @NgModule({
   declarations: [MainComponent],
-  imports: [CommonModule, MainRoutingModule, SharedModule],
+  imports: [
+    CommonModule,
+    MainRoutingModule,
+    SharedModule,
+    NzIconModule.forChild(icons),
+    NzToolTipModule,
+  ],
 })
 export class MainModule {}
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,42 +1,8 @@
-import {
-  animate,
-  state,
-  style,
-  transition,
-  trigger,
-} from '@angular/animations';
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  animations: [
-    trigger('displayMobNavLinks', [
-      state(
-        'start',
-        style({
-          right: '-100%',
-        })
-      ),
-      state(
-        'end',
-        style({
-          right: '0%',
-        })
-      ),
-      transition('start <=> end', [animate('.5s')]),
-    ]),
-  ],
 })
-export class HeaderComponent {
-  mobNavLinksState = 'start';
-
-  constructor(private router: Router) {}
-  toggleMobNavLinks(): void {
-    this.mobNavLinksState === 'start'
-      ? (this.mobNavLinksState = 'end')
-      : (this.mobNavLinksState = 'start');
-  }
-}
+export class HeaderComponent {}
