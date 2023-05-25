2ea11b1c1e71a4c6c63f0f3aba7bcf9e95abdcce
Patrik Bundula <patrik.bundula@gammaorg.hu>
2023-05-25 14:39:01 +0200

    get rid of unnecesarry content

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,18 +3,29 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NavLinksMobComponent } from './header/nav-links-mob/nav-links-mob.component';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, NavBarComponent, NavLinksMobComponent],
-  imports: [CommonModule, HttpClientModule, ToastrModule.forRoot()],
+  declarations: [HeaderComponent, NavBarComponent, NavLinksMobComponent],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    ToastrModule.forRoot(),
+    NzLayoutModule,
+    NzMenuModule,
+    NzBreadCrumbModule,
+  ],
   exports: [
     HttpClientModule,
     HeaderComponent,
-    FooterComponent,
     NavBarComponent,
+    NzLayoutModule,
+    NzMenuModule,
+    NzBreadCrumbModule,
   ],
 })
 export class SharedModule {}
