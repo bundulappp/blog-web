11e22557e47458dcd35151df3c213f3147161398
Patrik Bundula <patrik.bundula@gammaorg.hu>
2023-05-26 10:05:51 +0200

    add routing for BlogList component

--- src/app/feature/main/main-routing.module.ts
+++ src/app/feature/main/main-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main.component';
+import { BlogListComponent } from './blog-list/blog-list.component';
 
-const routes: Routes = [{ path: '', component: MainComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: '',
+        component: BlogListComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class MainRoutingModule { }
+export class MainRoutingModule {}
