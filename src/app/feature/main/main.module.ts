095da7138206c8f5145e63800eea17049851459a
Patrik Bundula <patrik.bundula@gammaorg.hu>
2023-05-26 09:50:36 +0200

    init blog list

--- src/app/feature/main/main.module.ts
+++ src/app/feature/main/main.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { BlogListComponent } from './blog-list/blog-list.component';
 
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [MainComponent, BlogListComponent],
   imports: [CommonModule, MainRoutingModule, SharedModule],
 })
 export class MainModule {}
