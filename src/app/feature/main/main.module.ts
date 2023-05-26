c020475b9bebd37be2b36d9013064bd15c3a78ce
Patrik Bundula <patrik.bundula@gammaorg.hu>
2023-05-26 09:52:17 +0200

    init blog item

--- src/app/feature/main/main.module.ts
+++ src/app/feature/main/main.module.ts
@@ -4,9 +4,10 @@ import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { BlogListComponent } from './blog-list/blog-list.component';
+import { BlogItemComponent } from './blog-list/blog-item/blog-item.component';
 
 @NgModule({
-  declarations: [MainComponent, BlogListComponent],
+  declarations: [MainComponent, BlogListComponent, BlogItemComponent],
   imports: [CommonModule, MainRoutingModule, SharedModule],
 })
 export class MainModule {}
