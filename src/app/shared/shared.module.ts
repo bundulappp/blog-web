1264f172ef3459fe9d1686716b4134b657106035
Patrik Bundula <bundulappp@gmail.com>
2023-05-26 07:15:32 +0200

    add login page

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,23 +4,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
 import { HeaderComponent } from './header/header.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { NavLinksMobComponent } from './header/nav-links-mob/nav-links-mob.component';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
-import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @NgModule({
-  declarations: [HeaderComponent, NavBarComponent, NavLinksMobComponent],
+  declarations: [HeaderComponent, NavBarComponent],
   imports: [
     CommonModule,
     HttpClientModule,
     ToastrModule.forRoot(),
     NzLayoutModule,
     NzMenuModule,
-    NzBreadCrumbModule,
     NzFormModule,
     NzInputModule,
     NzButtonModule,
@@ -31,7 +28,6 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
     NavBarComponent,
     NzLayoutModule,
     NzMenuModule,
-    NzBreadCrumbModule,
     NzFormModule,
     NzInputModule,
     NzButtonModule,
