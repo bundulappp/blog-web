89c27e8984eaf7596a88ec092a7576cbe2ee0311
Patrik Bundula <patrik.bundula@gammaorg.hu>
2023-06-06 14:10:09 +0200

    blog item add

--- src/app/feature/main/blog-list/blog-item/blog-item.component.ts
+++ src/app/feature/main/blog-list/blog-item/blog-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-blog-item',
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class BlogItemComponent {
   loading = false;
+  @Input() itemSample: any;
+
+ 
 }
