5945358b25736581e7e039694ffd67352489e6dc
Patrik Bundula <patrik.bundula@gammaorg.hu>
2023-06-07 15:54:03 +0200

    add register

--- src/app/feature/auth/register/register.component.ts
+++ src/app/feature/auth/register/register.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registrationFormGroup!: FormGroup;
 
+  constructor(
+    public authService: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
+
+  ngOnInit(): void {
+    this.registrationFormGroup = new FormGroup({
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      phoneNumber: new FormControl(''),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.pattern(
+          '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-ZáéíóúÁÉÍÓÚüűőŐÜ\\d]+$'
+        ),
+      ]),
+      checkPassword: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.pattern(
+          '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-ZáéíóúÁÉÍÓÚüűőŐÜ\\d]+$'
+        ),
+      ]),
+    });
+  }
+
+  registerUser(): void {
+    console.log('register...');
+  }
+
+  navigateToLogin(): void {
+    this.router.navigate(['/auth/login']);
+  }
 }
