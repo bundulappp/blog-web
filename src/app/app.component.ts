57e1e46c24c1534886d1da7a48562d95cde8cdc2
Patrik Bundula <patrik.bundula@gammaorg.hu>
2023-08-28 10:27:06 +0200

    make personal menu invisible if user is not auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './feature/auth/service/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  userInformation$ = this.authService.userInformation$;
+
+  constructor(private router: Router, private authService: AuthService) {}
+}
